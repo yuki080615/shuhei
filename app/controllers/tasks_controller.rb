212fec1e841f7113ba2ab4baeb8e2f388c57994a
yuki080615 <yuki080615>
2024-03-02 10:53:14 +0900

    タスクの新規作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,13 @@ class TasksController < ApplicationController
     def show
         @tasks = Task.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+        @task = Task.new(name: params[:task_name])
+        @task.save
+        redirect_to '/tasks'
+    end
 end
