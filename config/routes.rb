9b6978a123f81ee17628fe493b32a7c2fd236c78
yuki080615 <yuki080615>
2024-03-02 11:01:43 +0900

    routingを修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   root 'top#index'
   get '/tasks/new', to: 'tasks#new'
   get '/tasks' , to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task' 
   post '/tasks' , to: 'tasks#create'
+
 end
